304d672aecd251d6089ad78be96f457f9600278d
Lcj4696 <93701662+Lcj4696@users.noreply.github.com>
2023-06-02 17:13:00 +0900

    메인 리사이클러뷰 오류 수정

---
+++ app/src/main/java/com/example/myapplication11/ViewHolder4.java
@@ -0,0 +1,24 @@
+package com.example.myapplication11;
+
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+class ViewHolder4 extends RecyclerView.ViewHolder {
+    public TextView itemtitle_thirdsearch , itemtag_thirdsearch , score_thirdsearch;
+    public ImageView itemimage_thirdsearch , star_thirdsearch;
+
+    ViewHolder4(Context context, View itemView){
+        super(itemView);
+
+        itemimage_thirdsearch= itemView.findViewById(R.id.itemimage_thirdsearch);
+        itemtitle_thirdsearch= itemView.findViewById(R.id.itemtitle_thirdsearch);
+        score_thirdsearch= itemView.findViewById(R.id.score_thirdsearch);
+        star_thirdsearch= itemView.findViewById(R.id.star_thirdsearch);
+
+    }
+}
